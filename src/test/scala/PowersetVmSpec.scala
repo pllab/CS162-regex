055318d368259e9c99f14d4f10367bd95bb34f29
Nemanja Stojanovic <migimunz@gmail.com>
2018-11-08 21:01:54 -0800

    Added tests skeleton for PowersetVm

---
+++ src/test/scala/PowersetVmSpec.scala
@@ -0,0 +1,29 @@
+package edu.ucsb.cs.cs162.regex.vm
+
+import org.scalatest._
+import edu.ucsb.cs.cs162.regex._
+
+class PowersetVmSpec extends FlatSpec with Matchers {
+  //----------------------------------------------------------------------------
+  // Fixtures and helpers.
+  // ---------------------------------------------------------------------------
+
+  import Regex._
+
+  //----------------------------------------------------------------------------
+  // Tests.
+  // ---------------------------------------------------------------------------
+
+  behavior of "eval"
+
+  // Replace {language 1} with a more descriptive name for what you're testing.
+  // Feel free to add more tests, or write many shorter ones.
+
+  it should "parse strings in {language 1}" in { pending }
+
+  // more tests...
+
+  it should "not parse strings not in {language 1}" in { pending }
+
+  // more tests...
+}
